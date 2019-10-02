f71bbdeff86ed03cf6a6a25ceee311d035b99f93
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-02 12:27:48 +0200

    Update crea_dibujo3.py

--- crea_dibujo3.py
+++ crea_dibujo3.py
@@ -6,8 +6,7 @@ from tkinter import colorchooser
 ventana=Tk()
 ventana.geometry("860x800")
 ventana.configure(background="gray80")
-numLados=StringVar()
-grosor=StringVar()
+
 lista_colores=[]
 canvas = Canvas(master = ventana, width = 860, height = 735)
 canvas.pack()
@@ -28,11 +27,11 @@ def color(m):
 
 etiLados=Label(master=ventana,text="Numero Lados",bg="gray80")
 etiLados.place(x=1,y=744)
-entLados=Entry(master=ventana,textvariable=numLados)
+entLados=Entry(master=ventana)
 entLados.place(x=85,y=744)
 etiGrosor=Label(master=ventana,text="Grosor",bg="gray80")
 etiGrosor.place(x=220,y=744)
-entGrosor=Entry(master=ventana,textvariable=grosor)
+entGrosor=Entry(master=ventana)
 entGrosor.place(x=260,y=744)
 btnColor=Button(master=ventana,text="Color Pincel",bg="gray74",command=lambda:color("c"))
 btnColor.place(x=415,y=740)
@@ -44,3 +43,4 @@ btnGuardar=Button(master=ventana,text="Guardar",bg="gray74",width=21)
 btnGuardar.place(x=700,y=740)
 Button(master = ventana,text="Crear",bg="spring green",width=121).place(x=1,y=771)
 ventana.mainloop()
+
