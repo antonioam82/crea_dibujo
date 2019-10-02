9558800f25097774e98befe582c85ca438b55e62
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-03 00:32:39 +0200

    Update crea_dibujo3.py

--- crea_dibujo3.py
+++ crea_dibujo3.py
@@ -35,11 +35,14 @@ def hide():
         t.showturtle()
         st=True
 
+def clear():
+    t.reset()
+
 def crear():
     d=1
-    #print(entLados.get())
-    #t.color(lista_colores[0])
     for i in range(int(entLados.get())):
+        if len(lista_colores)>1:
+            t.color(lista_colores[i%(len(lista_colores))])
         t.left(int(entGrados.get()))
         t.fd(d)
         d+=1
@@ -64,14 +67,14 @@ btnColor=Button(master=ventana,text="Color Pincel",bg="gray74",command=lambda:co
 btnColor.place(x=415,y=740)
 btnFondo=Button(master=ventana,text="Color Fondo",bg="gray74",command=lambda:color("f"))
 btnFondo.place(x=520,y=740)
-btnClear=Button(master=ventana,text="Clear",bg="gray74")
+btnClear=Button(master=ventana,text="Clear",bg="gray74",command=clear)
 btnClear.place(x=739,y=740)
 btnGuardar=Button(master=ventana,text="Guardar",bg="gray74",width=10)
 btnGuardar.place(x=778,y=740)
 btnHide=Button(master=ventana,text="Hide/Show",bg="gray74",command=hide)
 btnHide.place(x=650,y=740)
 Button(master = ventana,text="Crear",bg="spring green",width=121,command=crear).place(x=1,y=771)
-
 ventana.mainloop()
+
 
 
